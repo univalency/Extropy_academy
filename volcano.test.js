2012b34eed52105d5d07eab5fc259f6dc35bc05f
univalency <70851370+univalency@users.noreply.github.com>
2021-09-13 18:06:47 +0100

    Update volcano.test.js

--- volcano.test.js
+++ volcano.test.js
@@ -2,16 +2,6 @@ const Volcano = artifacts.require("VolcanoToken");
 
 contract("Volcano", accounts => {
 
-    it("should mint 10000 VolcanoCoin", async () => {
-    const instance = await Volcano.deployed();
-    //console.log(instance);
-    const totalSupply = await instance.totalSupply.call();
-    assert.equal(
-        totalSupply.toNumber(),
-       10000,
-        "Minting Failed",
-        );            
-    });
 /* here is my test for checking zero balance of 0xcB7C09fEF1a308143D9bf328F2C33f33FaA46bC2" */
     it("should show zero balance", async () => {
         const instance = await Volcano.deployed();
